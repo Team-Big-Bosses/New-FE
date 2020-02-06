0bc2a747b0875628193b6b9b768675845dcae9c2
karenjli <karenjy.li@gmail.com>
2020-02-06 11:07:37 -0600

    proxyURL fix

--- client/src/components/landing/Login.js
+++ client/src/components/landing/Login.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import axios from "axios";
-import {Link} from 'react-router-dom'
-import '../../scss/Login.scss'
+import { Link } from "react-router-dom";
+import "../../scss/Login.scss";
 
 const Login = props => {
   const [credentials, setCredentials] = useState({
@@ -43,11 +43,26 @@ const Login = props => {
     <div className="register-container">
       <h1>Sign In to FlaskRPG!</h1>
       <form onSubmit={handleSubmit}>
-        <input type="text" name="username" placeholder="Username" onChange={handleChange} />
-        <input type="password" name="password" placeholder="Password" onChange={handleChange} />
+        <input
+          type="text"
+          name="username"
+          placeholder="Username"
+          onChange={handleChange}
+        />
+        <input
+          type="password"
+          name="password"
+          placeholder="Password"
+          onChange={handleChange}
+        />
         <button type="submit">Login</button>
       </form>
-      <p>Need an account? <Link className="link" to="/register">Sign up here</Link></p>
+      <p>
+        Need an account?{" "}
+        <Link className="link" to="/register">
+          Sign up here
+        </Link>
+      </p>
     </div>
   );
 };
