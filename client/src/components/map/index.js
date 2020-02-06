800089e790b6277194cc74358e90a181c693ecbf
Jenn Soderborg <52469600+jsoderborg7@users.noreply.github.com>
2020-02-06 01:16:26 -0700

    Revert "Revert "game integrated with landing page""

---
+++ client/src/components/map/index.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import store from '../../config/store'
+
+function MapTile(props) {
+    return <div className={`tile ${props.tile}`} />
+}
+
+function MapRow(props) {
+    return <div className="row">
+        {
+            props.tiles.map(tile => <MapTile tile={tile} />)
+        }
+    </div>
+}
+
+function Map(props) {
+    store.dispatch({ type: 'REVEAL_PLAYER' })
+    store.dispatch({ type: 'ENABLE_MOVEMENT' })
+
+    return (
+        <div
+            style={{
+                width: '320px',
+                height: '240px',     
+                backgroundColor: 'rgb(41, 38, 52)',
+                position: 'absolute'
+            }}>
+            {
+                props.tiles.map(row => <MapRow tiles={row} />)
+            }
+        </div>
+    )
+}
+
+export default Map
